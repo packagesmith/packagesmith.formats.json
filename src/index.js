3efd0084e5ce7513f3abc4e7b37fcb140c78b5c4
Keith Cirkel <github@keithcirkel.co.uk>
2016-02-15 20:02:30 +0000

    style: adhere to new eslint rules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-export default function jsonFile(process, { replacer = null, indent = 2 } = {}) {
+const defaultIndent = 2;
+export default function jsonFile(process, { replacer = null, indent = defaultIndent } = {}) {
   return (contents, ...rest) => JSON.stringify(process(JSON.parse(contents || '{}'), ...rest), replacer, indent);
 }
